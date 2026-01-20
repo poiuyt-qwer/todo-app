51f4d98622ef8c4eaf63fe8f3e06ac153958e8c0
Kwangil Jeon <goodjki11@gmail.com>
2026-01-20 16:12:12 +0700

    feat: make input box

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,6 +12,13 @@ const Todo = () => {
         <h1 className='text-3xl font-semibold'>To-Do List</h1>
     </div>
 
+{/* --------- input box ------------ */}
+
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
+        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+    </div>
+
     </div>
   )
 }
