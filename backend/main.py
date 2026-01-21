ab1c5265833f74e91caf32749a4d08795ba9cc00
Kwangil Jeon <goodjki11@gmail.com>
2026-01-21 17:26:49 +0700

    feat: add toggle, update and delete task

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,24 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class Task(SQLModel, table=True):
+class TaskBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
     title: str = Field()
+    
+class Task(TaskBase, table=True):
     is_complete: bool = Field(default=False)
+    # add datetime
+
+class TaskPublic(TaskBase):
+    is_complete: bool = Field(default=False)
+    
+class TaskCreate(TaskBase):
+    is_complete: bool = Field(default=False)
+    # add datetime
+    pass
+
+class TaskUpdate(TaskBase):
+    is_complete: bool | None = None
     
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -30,13 +44,47 @@ def on_startup():
     create_db_and_tables()
     
 @app.post("/tasks")
-def create_task(task: Task, session: SessionDep) -> Task:
-    session.add(task)
+def create_task(task: TaskCreate, session: SessionDep):
+    db_task = Task.model_validate(task)
+    session.add(db_task)
     session.commit()
-    session.refresh(task)
-    return task
+    session.refresh(db_task)
+    return db_task
 
 @app.get("/tasks")
 def read_tasks(session: SessionDep) -> list[Task]:
     tasks = session.exec(select(Task)).all()
     return tasks
+
+@app.patch("/tasks/{id}")
+def toggle_task_status(id: int, task: TaskUpdate, session: SessionDep):
+    task_db = session.get(Task, id)
+    if not task_db:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task_data = task.model_dump(exclude_unset=True)
+    task_db.sqlmodel_update(task_data)
+    session.add(task_db)
+    session.commit()
+    session.refresh(task_db)    
+    return task_db
+
+@app.put("/tasks/{id}")
+def update_task(id: int, task: TaskUpdate, session: SessionDep):
+    task_db = session.get(Task, id)
+    if not task_db:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task_data = task.model_dump(exclude_unset=True)
+    task_db.sqlmodel_update(task_data)
+    session.add(task_db)
+    session.commit()
+    session.refresh(task_db)    
+    return task_db
+    
+@app.delete("/tasks/{id}")
+def delete_task(id: int, session: SessionDep):
+    task = session.get(Task, id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    session.delete(task)
+    session.commit()
+    return {"ok": True}
